6290ff96b416110234ae3c53ba89c1c8cb0d19cb
Thomas Musselman <tmusselman@gmail.com>
2022-09-20 14:52:03 -0600

    change merchant invoices and dashboard route setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :merchants, except: [:index, :new, :edit, :update, :destroy] do
-    resources :dashboard, controller: :merchant_dashboard, only: [:index]
+  resources :merchants, only: [:show] do
+    get '/dashboard', to: 'merchants#show', as: :dashboard
     resources :items, except: [:destroy]
-    resources :invoices, controller: :merchant_invoices, only: [:index, :show]
+    resources :invoices, only: [:index, :show]
   end
   
   resources :invoice_items, only: [:update]
