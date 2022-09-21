dc4c455490298cdc1e442b9c916cc66993e63f58
Wyatt Kribs <kribswyatt@gmail.com>
2022-09-21 17:23:04 -0600

    adds poros test

---
+++ spec/poros/repo_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+require './app/facades/github_facade'
+
+RSpec.describe Repo do
+
+  it 'exits' do
+    repo = GitHubFacade.generate_repo
+    expect(repo).to be_a(Repo)
+  end
+
+end
