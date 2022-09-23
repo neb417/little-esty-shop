6e815c97e13bfac0547f4a63dcf2c0ce2f2a886c
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-23 15:06:33 -0600

    add migration for bulk discounts and make relationships between merchant and bulk discounts.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -3,6 +3,7 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoice_items, through: :items
   has_many :invoices, through: :items
+  has_many :bulk_discounts
 
   def distinct_invoices
     invoices.distinct
