33ad9b2f33ec0adf4a7f066871d91477c5d3306d
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-24 11:22:18 -0600

    add initial tests for bulk discount revenue.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def price_convert(integer_price)
     number_to_currency(integer_price / 100.to_d)
   end
+
+  def percentage_convert(percentage)
+    percentage.to_f/100
+  end
 end
