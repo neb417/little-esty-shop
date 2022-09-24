05b0c46774cd056dac6d0b0b239a67e4f327a99d
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-09-23 19:44:12 -0600

    add tests for validations, add validations to model for BulkDiscount

--- app/models/bulk_discount.rb
+++ app/models/bulk_discount.rb
@@ -1,4 +1,7 @@
 class BulkDiscount < ApplicationRecord
   belongs_to :merchant
-  
+
+  validates :name, presence: true
+  validates :threshold, numericality: true
+  validates :percentage, numericality: true
 end
